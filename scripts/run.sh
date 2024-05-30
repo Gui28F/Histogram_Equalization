316ae55440e5a1fde3a88dd8606f3813a70e0ddc
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2024-05-30 18:00:30 +0100

    change script to scripts folder
    Co-Authored-By: vladys <93454853+vladbug@users.noreply.github.com>

---
+++ scripts/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../..
+
+make
+echo "Sequential(n_iterations = 100):"
+echo "Average time for borabora_1.ppm photo:"
+./project ../../dataset/borabora_1.ppm 100 output.ppm 1
+
+echo "Average time for input01.ppm photo:"
+./project ../../dataset/input01.ppm 100 output.ppm 1
+
+echo "Average time for sample_5184×3456.ppm photo:"
+./project ../../dataset/sample_5184×3456.ppm 100 output.ppm 1
+
+echo "Parallel OMP(n_iterations = 100):"
+echo "Average time for borabora_1.ppm photo:"
+./project_par ../../dataset/borabora_1.ppm 100 output.ppm 16
+
+echo "Average time for input01.ppm photo:"
+./project_par ../../dataset/input01.ppm 100 output.ppm 16
+
+echo "Average time for sample_5184×3456.ppm photo:"
+./project_par ../../dataset/sample_5184×3456.ppm 100 output.ppm 16
+
+echo "Parallel CUDA(n_iterations = 100):"
+echo "Average time for borabora_1.ppm photo:"
+./project_par_cuda ../../dataset/borabora_1.ppm 100 output.ppm 16
+
+echo "Average time for input01.ppm photo:"
+./project_par_cuda ../../dataset/input01.ppm 100 output.ppm 16
+
+echo "Average time for sample_5184×3456.ppm photo:"
+./project_par_cuda ../../dataset/sample_5184×3456.ppm 100 output.ppm 16
